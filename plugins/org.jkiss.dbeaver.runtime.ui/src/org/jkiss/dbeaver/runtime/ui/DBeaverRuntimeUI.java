71185d439a961142502fba7fbf8db757c787fdf4
Alexander Fedorov <alexander.fedorov@jkiss.org>
2017-11-09 15:45:27 +0300

    #2458 add runtime.ui bundle

--- plugins/org.jkiss.dbeaver.runtime.ui/src/org/jkiss/dbeaver/runtime/ui/DBeaverRuntimeUI.java
+++ plugins/org.jkiss.dbeaver.runtime.ui/src/org/jkiss/dbeaver/runtime/ui/DBeaverRuntimeUI.java
@@ -1,7 +1,11 @@
 package org.jkiss.dbeaver.runtime.ui;
 
 public class DBeaverRuntimeUI {
+<<<<<<< Upstream, based on branch '2458-add-locale-selector' of git@github.com:serge-rider/dbeaver.git
 
+=======
+    
+>>>>>>> 8c6f1dd #2458 add runtime.ui bundle
     public static final String BUNDLE_SYMBOLIC_NAME = "org.jkiss.dbeaver.runtime.ui"; //$NON-NLS-1$
 
 }
